c2c53fd2d0b046fe0a89471db6f4ab21ec4c7697
ATIK_FAYSAL <atikfaysal1404@gmail.com>
2018-08-29 18:53:05 +0600

    29-08-18 add myFabJob,aboutUs,Feedback

---
+++ app/src/main/java/com/atik_faysal/diualumni/important/MyFabJob.java
@@ -0,0 +1,171 @@
+package com.atik_faysal.diualumni.important;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.atik_faysal.diualumni.R;
+import com.atik_faysal.diualumni.adapter.JobsAdapter;
+import com.atik_faysal.diualumni.background.PostInfoBackgroundTask;
+import com.atik_faysal.diualumni.background.SharedPreferencesData;
+import com.atik_faysal.diualumni.interfaces.Methods;
+import com.atik_faysal.diualumni.interfaces.OnResponseTask;
+import com.atik_faysal.diualumni.main.JobPortal;
+import com.atik_faysal.diualumni.models.JobsModel;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MyFabJob extends AppCompatActivity implements Methods
+{
+     private ProgressBar progressBar;
+     private RelativeLayout emptyView;
+     private TextView numOfResult,txtNoresult;
+
+     private RecyclerView recyclerView;
+     private LinearLayoutManager layoutManager;
+     private PostInfoBackgroundTask backgroundTask;
+     private CheckInternetConnection internetConnection;
+     private RequireMethods methods;
+     private DisplayMessage displayMessage;
+     private SharedPreferencesData sharedPreferencesData;
+     private JobPortal portal;
+
+
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.alumnis);
+          initComponent();
+     }
+
+
+     public void initComponent()
+     {
+          progressBar = findViewById(R.id.progressBar);
+          recyclerView = findViewById(R.id.lists);
+          emptyView = findViewById(R.id.emptyView);
+          txtNoresult = findViewById(R.id.txtNoResult);
+          numOfResult = findViewById(R.id.txtNumberOfRResult);
+          txtNoresult.setVisibility(View.INVISIBLE);
+          SwipeRefreshLayout refreshLayout = findViewById(R.id.refresh);
+          refreshLayout.setColorSchemeResources(R.color.color1,R.color.color1,R.color.color1);
+
+          layoutManager = new LinearLayoutManager(this);
+          backgroundTask = new PostInfoBackgroundTask(this,onResponseTask);
+          internetConnection = new CheckInternetConnection(this);
+          methods = new RequireMethods(this);
+          displayMessage = new DisplayMessage(this);
+          sharedPreferencesData = new SharedPreferencesData(this);
+          portal = new JobPortal();
+
+          //calling method
+          setToolbar();
+          retrieveMyFavJob();//retrieve my fav job
+          methods.reloadPage(refreshLayout,MyFabJob.class);
+     }
+
+     //set toolbar
+     @Override
+     public void setToolbar()
+     {
+          Toolbar toolbar = findViewById(R.id.toolbar);
+          setSupportActionBar(toolbar);toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+          getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+          getSupportActionBar().setDisplayShowHomeEnabled(true);
+          toolbar.setNavigationIcon(R.drawable.icon_back);
+          toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                    finish();
+               }
+          });
+     }
+
+     @Override
+     public void processJsonData(String jsonData) {
+
+     }
+
+     //get all job and current user favourite jobs
+     private void retrieveMyFavJob()
+     {
+          Map<String,String> maps = new HashMap<>();
+          maps.put("option","fabJob");
+          maps.put("stdId",sharedPreferencesData.getStudentId());
+          if(internetConnection.isOnline())
+               backgroundTask.InsertData(getString(R.string.readInfo),maps);
+          else displayMessage.errorMessage(getString(R.string.noInternet));
+     }
+
+     //view all job information in UI
+     private void viewJobInfo(String json)
+     {
+          final List<JobsModel> jobsModels = portal.processJson(json);
+
+          //add progress bar ...
+          final Timer timer = new Timer();
+          final Handler handler = new Handler();
+          final  Runnable runnable = new Runnable() {
+               @Override
+               public void run() {
+                    try {
+                         if(jobsModels.isEmpty())//if no jobs found
+                         {
+                              emptyView.setVisibility(View.VISIBLE);//empty view visible
+                              txtNoresult.setVisibility(View.VISIBLE);//no result text visible
+                              recyclerView.setVisibility(View.INVISIBLE);//list invisible
+                              numOfResult.setText("0");
+                         }
+                         else//if jobs found
+                         {
+                              emptyView.setVisibility(View.INVISIBLE);//empty view invisible
+                              recyclerView.setVisibility(View.VISIBLE);//no result text invisible
+                              JobsAdapter adapter = new JobsAdapter(MyFabJob.this, jobsModels,"fabJob");//create adapter
+                              recyclerView.setAdapter(adapter);//set adapter in recyler view
+                              layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                              recyclerView.setLayoutManager(layoutManager);
+                              recyclerView.setItemAnimator(new DefaultItemAnimator());
+                              numOfResult.setText(String.valueOf(jobsModels.size()));
+                         }
+                         progressBar.setVisibility(View.GONE);
+                         timer.cancel();
+                    }catch (NullPointerException e)
+                    {
+                         displayMessage.errorMessage(e.toString());
+                    }
+               }
+          };
+          timer.schedule(new TimerTask() {
+               @Override
+               public void run() {
+                    handler.post(runnable);
+               }
+          },2800);
+     }
+
+     //get json data from server
+     OnResponseTask onResponseTask = new OnResponseTask() {
+          @Override
+          public void onResultSuccess(final String value) {
+               runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                         viewJobInfo(value);
+                    }
+               });
+          }
+     };
+}
---
+++ app/src/main/java/com/atik_faysal/diualumni/others/Feedback.java
@@ -0,0 +1,172 @@
+package com.atik_faysal.diualumni.others;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.atik_faysal.diualumni.R;
+import com.atik_faysal.diualumni.background.PostInfoBackgroundTask;
+import com.atik_faysal.diualumni.background.SharedPreferencesData;
+import com.atik_faysal.diualumni.important.CheckInternetConnection;
+import com.atik_faysal.diualumni.important.DisplayMessage;
+import com.atik_faysal.diualumni.important.RequireMethods;
+import com.atik_faysal.diualumni.interfaces.OnResponseTask;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Created by USER on 1/29/2018.
+ */
+
+public class Feedback extends AppCompatActivity
+{
+        private EditText eFeedback;
+        private Button bFeedback;
+        private Toolbar toolbar;
+        private TextView txtName;
+
+        private DisplayMessage displayMessage;
+        private RequireMethods methods;
+        private CheckInternetConnection internetConnection;
+
+        private String currentUser;
+        //private static final String FILE_URL = "http://192.168.56.1/feedback.php";
+
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.feedback);
+                initComponent();
+        }
+
+        //initialize all user information related variable by getText from textView or editText
+        @SuppressLint("ClickableViewAccessibility")
+        private void initComponent()
+        {
+                eFeedback = findViewById(R.id.eFeedback);
+                txtName = findViewById(R.id.txtName);
+                bFeedback = findViewById(R.id.bFeedback);
+                bFeedback.setBackgroundDrawable(getDrawable(R.drawable.disable_button));
+                TextView txtDate = findViewById(R.id.txtDate);
+                toolbar = findViewById(R.id.toolbar1);
+                setSupportActionBar(toolbar);
+
+                displayMessage = new DisplayMessage(this);
+                methods = new RequireMethods(this);
+                SharedPreferencesData sharedPreferenceData = new SharedPreferencesData(this);
+                internetConnection = new CheckInternetConnection(this);
+                currentUser = sharedPreferenceData.getStudentId();
+
+                txtName.setText(sharedPreferenceData.getUserName());
+                txtDate.setText(methods.getDateWithTime());
+                if(eFeedback.getText().toString().isEmpty())bFeedback.setEnabled(false);
+
+                //on text changed listener and take action
+                eFeedback.addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+                                if(eFeedback.getText().toString().length()<20)
+                                {
+                                        bFeedback.setEnabled(false);
+                                        bFeedback.setBackgroundDrawable(getDrawable(R.drawable.disable_button));
+                                }
+                                else
+                                {
+                                        bFeedback.setEnabled(true);
+                                        bFeedback.setBackgroundDrawable(getDrawable(R.drawable.button_done));
+                                }
+                        }
+                });
+
+                //calling method
+                onButtonClick();//button click method
+                setToolbar();//set toolbar in top
+        }
+
+        //set a toolbar,above the page
+        private void setToolbar()
+        {
+                toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                getSupportActionBar().setDisplayShowHomeEnabled(true);
+                toolbar.setNavigationIcon(R.drawable.icon_back);
+                toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                finish();
+                        }
+                });
+        }
+
+        //button click
+        private void onButtonClick()
+        {
+                bFeedback.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                if(internetConnection.isOnline())
+                                {
+                                        Map<String,String> map = new HashMap<>();
+                                        map.put("option","feedback");
+                                        map.put("stdId",currentUser);
+                                        map.put("feedback",eFeedback.getText().toString());
+                                        map.put("date",methods.getDateWithTime());
+                                        PostInfoBackgroundTask backgroundTask = new PostInfoBackgroundTask(Feedback.this,responseTask);
+                                        backgroundTask.InsertData(getResources().getString(R.string.otherInsertion),map);
+                                }else displayMessage.errorMessage(getResources().getString(R.string.noInternet));
+
+                        }
+                });
+
+                txtName.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                                Intent page = new Intent(Feedback.this,Feedback.class);
+                                page.putExtra("userName",currentUser);
+                                startActivity(page);
+                        }
+                });
+
+        }
+
+        //get server response for post feedback
+        OnResponseTask responseTask = new OnResponseTask() {
+                @Override
+                public void onResultSuccess(final String message) {
+                        runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                        switch (message)
+                                        {
+                                                case "success":
+                                                        Toast.makeText(Feedback.this,"Your feedback is added.",Toast.LENGTH_LONG).show();
+                                                        finish();
+                                                        break;
+                                                default:
+                                                        displayMessage.errorMessage(getResources().getString(R.string.executionFailed));
+                                                        break;
+                                        }
+                                }
+                        });
+                }
+        };
+
+}
