164d76bbe9c6791415911d93a5965ee3f5e46478
ATIK_FAYSAL <atikfaysal1404@gmail.com>
2018-09-13 16:55:47 +0600

    13-09-18 add a popup menu with edit and delete edit and delete job

---
+++ app/src/main/java/com/atik_faysal/diualumni/others/EditJobPost.java
@@ -0,0 +1,260 @@
+package com.atik_faysal.diualumni.others;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.atik_faysal.diualumni.R;
+import com.atik_faysal.diualumni.background.PostInfoBackgroundTask;
+import com.atik_faysal.diualumni.background.SharedPreferencesData;
+import com.atik_faysal.diualumni.important.CheckInternetConnection;
+import com.atik_faysal.diualumni.important.DisplayMessage;
+import com.atik_faysal.diualumni.important.RequireMethods;
+import com.atik_faysal.diualumni.interfaces.OnResponseTask;
+import com.atik_faysal.diualumni.main.JobPortal;
+import com.atik_faysal.diualumni.main.PostNewJob;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EditJobPost extends PostNewJob
+{
+     private Map<String,String>infoMap;
+
+     @Override
+     protected void onCreate(@Nullable Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+     }
+
+     //initialize all component
+     @SuppressLint("SetTextI18n")
+     @Override
+     public void initComponent()
+     {
+          txtTitle = findViewById(R.id.txtTitle);
+          txtDes = findViewById(R.id.txtDes);
+          txtEdu = findViewById(R.id.txtEdu);
+          txtReq = findViewById(R.id.txtReq);
+          txtSalary = findViewById(R.id.txtSalary);
+          txtPhone = findViewById(R.id.txtPhone);
+          txtCompany = findViewById(R.id.txtSource);
+          txtEmail = findViewById(R.id.txtEmail);
+          sCategory = findViewById(R.id.sCategory);
+          sLocation = findViewById(R.id.sLocation);
+          TextView txtLink = findViewById(R.id.txtLink);
+          Button bDone = findViewById(R.id.bDone);
+          txtDeadLine = findViewById(R.id.txtDeadLine);
+          progressBar = findViewById(R.id.progress);
+          txtExp = findViewById(R.id.txtExp);
+          RadioButton rContact = findViewById(R.id.rContract);
+          RadioButton rOthers = findViewById(R.id.rOther);
+          RadioButton rFullTime = findViewById(R.id.rFullTime);
+          RadioButton rPartTime = findViewById(R.id.rPartTime);
+
+          infoMap = new HashMap<>();
+          infoMap = (Map<String, String>) getIntent().getSerializableExtra("maps");
+          txtTitle.setText(infoMap.get("title"));
+          txtDes.setText(infoMap.get("des"));
+          txtEdu.setText(infoMap.get("edu"));
+          txtReq.setText(infoMap.get("req"));
+          txtExp.setText(infoMap.get("expe"));
+          txtDeadLine.setText(infoMap.get("deadLine"));
+          txtSalary.setText(infoMap.get("salary"));
+          txtCompany.setText(infoMap.get("company"));
+          txtPhone.setText(infoMap.get("phone"));
+          txtEmail.setText(infoMap.get("email"));
+          switch (infoMap.get("type")) {
+               case "full time":
+                    rFullTime.setChecked(true);
+                    rPartTime.setChecked(false);
+                    rOthers.setChecked(false);
+                    rContact.setChecked(false);
+                    jobType="full time";
+                    break;
+               case "part time":
+                    rFullTime.setChecked(false);
+                    rPartTime.setChecked(true);
+                    rOthers.setChecked(false);
+                    rContact.setChecked(false);
+                    jobType="part time";
+                    break;
+               case "other":
+                    rFullTime.setChecked(false);
+                    rPartTime.setChecked(false);
+                    rOthers.setChecked(true);
+                    rContact.setChecked(false);
+                    jobType="other";
+                    break;
+               case "contract":
+                    rFullTime.setChecked(false);
+                    rPartTime.setChecked(false);
+                    rOthers.setChecked(false);
+                    rContact.setChecked(true);
+                    jobType="contract";
+                    break;
+          }
+
+          //set click listener
+          bDone.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                    updatePost();
+               }
+          });
+          txtLink.setOnClickListener(this);
+          txtDeadLine.setOnClickListener(this);
+          bDone.setText("Save changes");//change text for update
+
+          displayMessage = new DisplayMessage(this);
+          internetConnection = new CheckInternetConnection(this);
+          backgroundTask = new PostInfoBackgroundTask(this,onResponseTask);
+          sharedPreferencesData = new SharedPreferencesData(this);
+          methods = new RequireMethods(this);
+          Calendar calendar = Calendar.getInstance();
+
+          day = calendar.get(Calendar.DAY_OF_MONTH);
+          month = calendar.get(Calendar.MONTH);
+          year = calendar.get(Calendar.YEAR);
+
+          super.setToolbar();
+          super.textChangeListener();
+
+          spinnerCategory();
+          spinnerLocation();
+     }
+
+     //update job post
+     private void updatePost()
+     {
+          if(super.dataValidator())
+          {
+               Map<String,String> maps = new HashMap<>();
+               maps.put("option","update");
+               maps.put("id",infoMap.get("id"));
+               maps.put("email",email);
+               maps.put("phone",phone);
+               maps.put("type",jobType);
+               maps.put("title",title);
+               maps.put("des",description);
+               maps.put("edu",education);
+               maps.put("req",requirement);
+               maps.put("expe",experience);
+               maps.put("category",category);
+               maps.put("loc",division);
+               maps.put("company",company);
+               maps.put("deadLine",deadLine);
+               maps.put("salary",salary);
+
+               if(internetConnection.isOnline())
+                    backgroundTask.InsertData(getString(R.string.postJob),maps);
+               else displayMessage.errorMessage(getResources().getString(R.string.noInternet));
+          }else displayMessage.errorMessage(getString(R.string.infoErr));
+     }
+
+     //set job location in spinner
+     protected void spinnerLocation()
+     {
+          ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.division,R.layout.support_simple_spinner_dropdown_item);
+          adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+          sLocation.setAdapter(adapter);
+          int pos = adapter.getPosition(infoMap.get("city"));
+          sLocation.setSelection(pos);
+          sLocation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+               @Override
+               public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
+                    division = parent.getItemAtPosition(i).toString();
+               }
+
+               @Override
+               public void onNothingSelected(AdapterView<?> adapterView) {}
+          });
+     }
+
+     //set job category in spinner
+     protected void spinnerCategory()
+     {
+          ArrayAdapter<CharSequence>adapter = ArrayAdapter.createFromResource(this,R.array.category,R.layout.support_simple_spinner_dropdown_item);
+          adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+          sCategory.setAdapter(adapter);
+          int pos = adapter.getPosition(infoMap.get("category"));
+          sCategory.setSelection(pos);
+          sCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+               @Override
+               public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
+                    String item = parent.getItemAtPosition(i).toString();
+                    if(!item.equals("Select a category"))
+                         category = item;
+               }
+
+               @Override
+               public void onNothingSelected(AdapterView<?> adapterView) {}
+          });
+     }
+
+     //define member type
+     protected void chooseJobNature(View view)
+     {
+          boolean checked = ((RadioButton)view).isChecked();
+
+          switch (view.getId())
+          {
+               case R.id.rFullTime:
+                    if(checked)jobType="full time";
+                    break;
+
+               case R.id.rPartTime:
+                    if(checked)jobType="part time";
+                    break;
+
+               case R.id.rOther:
+                    if(checked)jobType="other";
+                    break;
+
+               case R.id.rContract:
+                    if(checked)jobType="contract";
+                    break;
+          }
+     }
+
+     private OnResponseTask onResponseTask = new OnResponseTask() {
+          @Override
+          public void onResultSuccess(final String value) {
+               runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                         if(value.equals("success"))
+                         {
+                              progressBar.setVisibility(View.VISIBLE);
+                              Thread thread = new Thread(new Runnable() {
+                                   @Override
+                                   public void run() {
+                                        try
+                                        {
+                                             Thread.sleep(1500);
+                                             methods.closeActivity(EditJobPost.this,JobPortal.class);
+                                        }catch (InterruptedException e)
+                                        {
+                                             e.printStackTrace();
+                                        }
+                                   }
+                              });
+                              thread.start();
+                         }
+                         else displayMessage.errorMessage(getResources().getString(R.string.executionFailed));
+                    }
+               });
+          }
+     };
+}
