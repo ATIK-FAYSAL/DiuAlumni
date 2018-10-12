ffdc73d4c2351b231738f7cbd66f7ec7f790ffe0
ATIK_FAYSAL <atikfaysal1404@gmail.com>
2018-10-13 00:56:27 +0600

    13-10-18 add message chat feature

---
+++ app/src/main/java/com/atik_faysal/diualumni/messages/AllMessages.java
@@ -0,0 +1,16 @@
+package com.atik_faysal.diualumni.messages;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.atik_faysal.diualumni.R;
+
+public class AllMessages extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alumnis);
+    }
+}
